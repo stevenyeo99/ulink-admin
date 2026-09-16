fe3578bbfeaa7dea7ea09fc5dae2d4f375672c71
steven.yeo <steven.yeo@dynrtech.com>
2026-09-16 09:26:05 +0700

    Add Spacing to document checklist template.

--- src/ulink-api/modules/email-sender/templates.js
+++ src/ulink-api/modules/email-sender/templates.js
@@ -42,12 +42,16 @@ ULINK AI Bot`;
 const MISSING_DOCUMENTS_INTRO = `Dear Valued Customer,
 
 Please be informed that in order to process the claim that have submitted, the following information needs to be completed:
+
 `;
 
 const MISSING_DOCUMENTS_FOOTER = `
 Please re-submit the required document(s) in order to start the claims process. Should you have any questions, kindly contact us at ayahealthinfo@ayasompo.com or call our hotline during office hours.
+
 Please be note that claim might be rejected in event that they do not meet the requirements regardless of requesting for the additional documents.
+
 Please note that if all the necessary documents supporting your claim have been satisfactorily submitted, we will notify you of the claim outcomes in 2-7 business days for small claims and 7-10 business days for large claims on average.
+
 ${SIGN_OFF}`;
 
 const DOCUMENT_COMPLETE_ACK_BODY = `Dear Valued Customer,
